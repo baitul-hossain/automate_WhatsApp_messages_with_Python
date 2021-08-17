9d4b542b42c80ce8ae515e75ca1ae54f9dc95989
Baitul Hossain <88625903+baitul-hossain@users.noreply.github.com>
2021-08-17 03:06:02 +0100

    updated

---
+++ main.py
@@ -0,0 +1,10 @@
+# Install pywhatkit -- pip install pywhatkit
+
+import pywhatkit as pwk
+
+# (phone number, message, time hour, time minute)
+pwk.sendwhatmsg("+44 7784 054157", "Reminder for meeting ...", 10, 30)
+
+
+# Run this script and the message will be sent at the time specified 10:30
+
